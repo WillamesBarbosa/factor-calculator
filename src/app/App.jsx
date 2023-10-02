a9206306ad0233d52c85603e5c5e6a33703c7a76
Willames <willamessilva1999@gmail.com>
2023-10-01 21:54:02 -0300

    refactor: include button into app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Button from './components/button/button';
+import Container from './components/container/container';
+
+const array1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', '0', 'V'];
+const array2 = ['C', ','];
 
 function App() {
   return (
-    <div className="App">Hello word</div>
+    <Container>
+      <Button texts={array1} />
+      <Button texts={array2} />
+    </Container>
+
   );
 }
 
