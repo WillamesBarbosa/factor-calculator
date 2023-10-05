a52bbc2c23c190f101be91530ee7adf94ea4fd1e
Willames <willamessilva1999@gmail.com>
2023-10-04 21:47:11 -0300

    refactor: insert globalstyles calculatorbody and portion into app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { GlobalStyles } from './styles/global-styles';
+
 import { Screen } from './components/screen/screen';
 import Button from './components/button/button';
 import Container from './components/container/container';
+import { CalculatorBody } from './components/calculator_body/calculatorBody';
+import { Portion } from './components/portion/portion';
 
 const array1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', '0', 'V'];
 const array2 = ['C', ','];
 
 function App() {
   return (
-    <>
+    <CalculatorBody>
       <Screen />
+      <Portion amount={12} />
       <Container>
         <Button texts={array1} />
         <Button texts={array2} />
       </Container>
-    </>
-
+      <GlobalStyles />
+    </CalculatorBody>
   );
 }
 
