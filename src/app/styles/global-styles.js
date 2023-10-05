4077cd13b6652ad1282ed3db9b64c6be42c21608
Willames <willamessilva1999@gmail.com>
2023-10-04 21:44:13 -0300

    feat: create global-styles

---
+++ src/app/styles/global-styles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *   {
+    margin: 0;
+    padding: 0;
+  }
+  body{
+    background-color: rgba(23, 24, 26, 1);
+  }
+`;
