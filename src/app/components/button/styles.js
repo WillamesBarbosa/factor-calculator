023bc47e1e1e2e80f1614a44ed20d809ea0e7435
Willames <willamessilva1999@gmail.com>
2023-10-06 22:14:52 -0300

    styles: remove styles not used into button

--- src/app/components/button/styles.js
+++ src/app/components/button/styles.js
@@ -1,15 +1,5 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Button = styled.button`
   margin: 0;
-`;
-
-export const Container = styled.div`
-  ${(props) => css`
-    flex: ${props.size > 2 ? '70%' : '30%'};
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: ${props.size > 2 ? 'repeat(3, 1fr)' : 'repeat(1, 1fr)'};
-  `}
 `;
