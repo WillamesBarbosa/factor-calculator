01fb70629aa817c612cb1794acf2b7ff97e8939b
Willames <willamessilva1999@gmail.com>
2023-10-06 22:17:56 -0300

    feat: insert into app all functions

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyles } from './styles/global-styles';
 
 import { Screen } from './components/screen/screen';
@@ -6,18 +6,36 @@ import Button from './components/button/button';
 import Container from './components/container/container';
 import { CalculatorBody } from './components/calculator_body/calculatorBody';
 import { Portion } from './components/portion/portion';
+import { ButtonContainer } from './components/button-container/buttonContainer';
+import { handleInsertIntoDisplay } from './services/handleInsertIntoDisplay';
+import { handleSelectValue } from './services/handleSelectValue';
+import factorCalculatorService from './services/factorCalculatorService';
+import { handleDeleteNumbersIntoDisplay } from './services/handleDeleteNumbersIntoDisplay';
 
-const array1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', '0', 'V'];
-const array2 = ['C', ','];
+// const [sumValue, setSumValue] = useState(0);
 
 function App() {
+  const [initialValue, setInitialValue] = useState('0');
+  const [selectValue, setSelectValue] = useState(0);
   return (
     <CalculatorBody>
-      <Screen />
-      <Portion amount={12} />
+      <Screen text={`R$ ${initialValue === '' ? '0' : initialValue}`} />
+      <Portion amount={12} onChange={(event) => handleSelectValue(event, setSelectValue)} />
       <Container>
-        <Button texts={array1} />
-        <Button texts={array2} />
+        <ButtonContainer>
+          <Button texts="1" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="2" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="3" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="4" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="5" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="6" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="7" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="8" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="9" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="delete" onClick={() => handleDeleteNumbersIntoDisplay(initialValue, setInitialValue)} />
+          <Button texts="0" onClick={(event) => handleInsertIntoDisplay(event, initialValue, setInitialValue)} />
+          <Button texts="=" onClick={() => factorCalculatorService(initialValue, selectValue, setInitialValue)} />
+        </ButtonContainer>
       </Container>
       <GlobalStyles />
     </CalculatorBody>
