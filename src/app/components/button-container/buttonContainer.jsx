8e8175380b23dabca41e42a228a2f9b260638d34
Willames <willamessilva1999@gmail.com>
2023-10-10 23:57:57 -0300

    refactor: exclude to reorganize code

--- src/app/components/button-container/buttonContainer.jsx
+++
@@ -1,8 +0,0 @@
-import P from 'prop-types';
-import * as Styled from './styles';
-
-export const ButtonContainer = ({ children }) => (<Styled.Container>{children}</Styled.Container>);
-
-ButtonContainer.propTypes = {
-  children: P.node.isRequired,
-};
